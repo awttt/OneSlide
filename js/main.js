f62e539e5f6f19d05ce32fd63df658818f5f44f5
awttt <350944364@qq.com>
2020-07-27 19:50:40 +0800

    主题切换功能

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+
+const $ = s => document.querySelector(s)
+const $$ = s => document.querySelectorAll(s)
 const isMain = str => (/^#{1,2}(?!#)/).test(str)
 const isSub = str => (/^#{3}(?!#)/).test(str)
 
 function convert(raw) {
-  let arr = raw.split(/\n(?=\s*#)/).filter(s => s!="").map(s => s.trim())
+  let arr = raw.split(/\n(?=\s*#)/).filter(s => s !== "").map(s => s.trim())
 
   let html = ''
   for(let i=0; i<arr.length; i++) {
@@ -70,24 +73,104 @@ ${arr[i]}
   return html
 }
 
-function loadMarkdown(raw) {
-    localStorage.markdown = raw
-    location.reload()
+
+const Menu = {
+init(){
+  console.log('menu')
+  this.$settingIcon = $('.control .icon-setting')
+  this.$menu  = $('.menu')
+  this.$closeIcon = $('.menu .icon-close')
+  this.$$tabs = $$('.menu .tab')
+  this.$$contents = $$('.menu .content')
+
+  this.bind()
+},
+
+  bind(){
+  this.$settingIcon.onclick =()=> {
+    this.$menu.classList.add('open')
+   }
+   this.$closeIcon.onclick=()=>{
+    this.$menu.classList.remove('open')
+   }
+   this.$$tabs.forEach($tab => $tab.onclick = ()=> {
+     this.$$tabs.forEach($node=>$node.classList.remove('active'))
+     $tab.classList.add('active')
+     let index = [...this.$$tabs].indexOf($tab)
+     this.$$contents.forEach($node=>$node.classList.remove('active'))
+     this.$$contents[index].classList.add('active')
+   })
+  }
 }
 
-function start() {
-    let TPL = `# One Slide`
-    let html = convert(localStorage.markdown || TPL)
-    document.querySelector('.slides').innerHTML = html
+const Editor = {
+  init(){
+    console.log('editor')
+    this.$editInput = $('.edit textarea')
+    this.$saveBtn = $('.edit .button-save')
+    this.markdown = localStorage.markdown || `# one slide`
+    this.$slideContainer = $('.slides')
+    this.bind()
+    this.start()
+  },
+  bind(){
+    this.$saveBtn.onclick = () => {
+      localStorage.markdown = this.$editInput.value
+      location.reload()
+    }
+  },
+  start(){
+    this.$editInput.value = this.markdown
+    this.$slideContainer.innerHTML = convert(this.markdown)
     Reveal.initialize({
-                controls: true,
-                progress: true,
-                center: true,
-                hash: true,
+      controls: true,
+      progress: true,
+      center: true,
+      hash: true,
 
-                // Learn about plugins: https://revealjs.com/plugins/
-                plugins: [ RevealZoom, RevealNotes, RevealSearch, RevealMarkdown, RevealHighlight ]
-            });
+      // Learn about plugins: https://revealjs.com/plugins/
+      plugins: [ RevealZoom, RevealNotes, RevealSearch, RevealMarkdown, RevealHighlight ]
+    });
+  }
+
 }
 
-start()
+const Theme = {
+  init(){
+
+    this.$$figures = $$('.themes figure')
+    this.bind()
+    this.loadTheme()
+  },
+
+  bind(){
+    this.$$figures.forEach($figure=>$figure.onclick=()=>{
+      this.$$figures.forEach($item=>$item.classList.remove('select'))
+      $figure.classList.add('select')
+      this.setTheme($figure.dataset.theme)
+    })
+  },
+  setTheme(theme){
+    localStorage.theme = theme
+    location.reload()
+  },
+  loadTheme(){
+    let theme = localStorage.theme || 'black'
+    let $link = document.createElement('link')
+    $link.rel = 'stylesheet'
+    $link.href = `dist/theme/${theme}.css`
+    document.head.appendChild($link)
+  }
+}
+
+const App = {
+init (){
+  [...arguments].forEach(Module=>Module.init())
+}
+}
+
+
+App.init(Menu,Editor,Theme)
+
+
+
